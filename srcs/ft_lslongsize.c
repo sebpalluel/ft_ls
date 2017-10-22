0f86ce6db93c2c2fc7912a2aa4400ee00729bed2
sebpalluel <sebpalluel@free.fr>
2017-10-22 16:18:30 +0200

    resolved for good issue in /dev and /dev/fd, now size locked to 5 or 7 in either case

--- srcs/ft_lslongsize.c
+++ srcs/ft_lslongsize.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/19 18:10:39 by psebasti          #+#    #+#             */
-/*   Updated: 2017/10/22 15:29:51 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/10/22 16:17:56 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,14 @@ void				ft_lsdispsize(t_disp_size *size, t_obj *current)
 	size->size = ft_get_max_size(ft_intlen(current->st_size), size->size);
 	size->total += current->st_blocks;
 	ft_lsdisp_usrgrp_size(size, current);
-	if ((size->maj + size->min) > 0 && \
+	if (ft_strncmp("/dev/", current->path, 4) == OK && \
 			(S_ISLNK(current->st_mode) || S_ISDIR(current->st_mode)))
-		size->size = size->min + size->maj + 2;
+	{
+		if(ft_strncmp("/dev/fd/", current->path, 8) == OK)
+			size->size = 5;
+		else
+			size->size = 7;
+	}
 }
 
 t_disp_size			ft_lslongsize(t_arg arg, t_obj *files)
